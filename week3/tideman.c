5d374f6a1f0feccefda9a6a696da2306659f45e7
MakaRonish <ronishmakaju12345@gmail.com>
2025-12-28 21:36:47 -0500

    Update tideman.c

--- week3/tideman.c
+++ week3/tideman.c
@@ -205,5 +205,6 @@ int main(void)
         printf("Results Sorted answer\n");
         printf("Results Sorted answer\n");
         printf("votes %s against %s\nvotes total number: %d\n ", arrange_result[i].name, arrange_result[i].won_against, arrange_result[i].vote);
+        printf("votes %s against %s\nvotes total number: %d\n ", arrange_result[i].name, arrange_result[i].won_against, arrange_result[i].vote);
     }
 }
